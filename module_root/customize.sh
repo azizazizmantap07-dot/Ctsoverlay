21ae82621219640d2dc1c68c6ea219480f8059f3
azizazizmantap07-dot <azizazizmantap07@gmail.com>
2026-09-12 10:31:24 +0700

    update repo

--- module_root/customize.sh
+++
@@ -1,6 +0,0 @@
-#!/system/bin/sh
-# Dijalankan Manager (Magisk/KernelSU) saat instalasi module.
-ui_print "- Circle Search Overlay: Assistant Setup"
-ui_print "  Module ini hanya mendaftarkan app sebagai default Assistant."
-ui_print "  Pastikan APK Circle Search Overlay sudah terpasang lebih dulu."
-set_perm "$MODPATH/service.sh" 0 0 0755
